1e0184028ef0c8087d6ca8dec079594e18e6cc2c
Rafael Gamboa <gpr0028293@est.univalle.edu>
2024-04-19 21:07:09 -0400

    interfaz login

--- CB-24-Proyecto-FAB-Frontend/src/main.tsx
+++ CB-24-Proyecto-FAB-Frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './components/user/login/login.tsx';
+
+const router = createBrowserRouter([
+  { path:  "/",  element: <Login /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <RouterProvider router={router}/>
+  </React.StrictMode>
 )
+
