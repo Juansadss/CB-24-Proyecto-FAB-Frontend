e1c8b2ced089fa09e6e2b64c0ae6075f6a66abc3
Manuel Villca <vcj0028559@est.univalle.edu>
2024-04-23 18:37:49 -0400

    Menu vertical SARFAB

---
+++ CB-24-Proyecto-FAB-Frontend/src/components/sar/guard-management/guard-management.tsx
@@ -0,0 +1,11 @@
+import LayoutSar from "../layout-sar/layout-sar"
+import "./guard-management.css"
+
+export default function GuardManagement() {
+    
+    return(
+        <LayoutSar>
+            <h2>Gestion Guardias </h2>
+        </LayoutSar>
+    );    
+}
--- CB-24-Proyecto-FAB-Frontend/src/main.tsx
+++ CB-24-Proyecto-FAB-Frontend/src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Login from './components/user/login/login.tsx';
 import MainMenu from './components/main-menu/main-menu.tsx';
+import GuardManagement from './components/sar/guard-management/guard-management.tsx';
 
 const router = createBrowserRouter([
   { path:  "/",  element: <Login /> },
   { path:  "/menu-principal",  element: <MainMenu /> },
+  { path:  "/sar/gestion-guardias",  element: <GuardManagement /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
