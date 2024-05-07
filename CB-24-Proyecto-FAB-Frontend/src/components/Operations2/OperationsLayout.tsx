8be59b0e93a3f933697bff573acee08ec2fc9eae
alejandropariente <pma0031712@est.univalle.edu>
2024-05-07 10:42:33 -0400

    Layout Integrado

--- CB-24-Proyecto-FAB-Frontend/src/components/Operations2/OperationsLayout.tsx
+++ CB-24-Proyecto-FAB-Frontend/src/components/Operations2/OperationsLayout.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import './Performance.css';
 import '../main-menu/main-menu.css';
 import '../../index.css';
 import './OperationsLayout.css';
+import Logo from './Images/LogoSarFAb.png';
+import { MdHome } from "react-icons/md";
+import { FaClipboardList } from "react-icons/fa";
+import { BiSolidAlarmExclamation } from "react-icons/bi";
+import { FaColumns } from "react-icons/fa";
+import { FaPlaneUp } from "react-icons/fa6";
+import { IoIosLogOut } from "react-icons/io";
 import Title from './WindowTitle';
 interface LayoutProps {
     childComponent: React.ComponentType<any>;
@@ -11,9 +19,54 @@ interface LayoutProps {
 const OperationsLayout: React.FC<LayoutProps> = ({ childComponent: ChildComponent, message }) => {
     return (
         <div className='flex layout-container'>
-            <div className='side-bar-container'>
+            <div className="side-menu-section">
+                <div>
+                    <img src={Logo} className="logoSarFab" alt="" />
+                    <h1>Fuerza Aerea</h1>
+                </div>
+                <div className="sideMenu">
+                    <div>
+                        <Link to={'/Operaciones/Performance'}>
+                            <MdHome size={'2.5em'}></MdHome>
+                            <h5>Perfil</h5>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={'/Operaciones/FlightRequest'}>
+                            <FaClipboardList size={'2.5em'}></FaClipboardList>
+                            <h5>Solicitudes Vuelos</h5>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={'/Operaciones/Performance'}>
+                            <BiSolidAlarmExclamation size={'2.5em'}></BiSolidAlarmExclamation>
+                            <h5>Matrices De Riesgo</h5>
+                        </Link>
+                        
+                    </div>
+                    <div>
+                        <Link to={'/Operaciones/Performance'}>
+                            <FaColumns size={'2.5em'}></FaColumns>
+                            <h5>Tablas De Riesgo</h5>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={'/Operaciones/Requerimientos-Aeronave'}>
+                            <FaPlaneUp size={'2.5em'}></FaPlaneUp>
+                            <h5>Pilotos</h5>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={'/'}>
+                            <IoIosLogOut size={'2.5em'}></IoIosLogOut>
+                            <h5>Cerrar Sesion</h5>
+                        </Link>
+                    </div>
+
+                </div>
 
             </div>
+
             <div className='container-form'>
                 <Title title={message}></Title>
                 <ChildComponent></ChildComponent>
