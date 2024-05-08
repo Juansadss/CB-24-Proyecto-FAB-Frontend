9a8729a4897c15e28a503c078a8b3ba7ac177f55
Manuel Villca <vcj0028559@est.univalle.edu>
2024-05-08 19:14:52 -0400

    agregando boton nuevo guardia

---
+++ CB-24-Proyecto-FAB-Frontend/src/components/sar/create-guard-modal/create-guard-modal.tsx
@@ -0,0 +1,51 @@
+import { FaAngleLeft } from "react-icons/fa6";
+import "./create-guard-modal.css"
+import { useState } from "react";
+
+interface CreateGuardModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export default function CreateGuardModal({ isOpen, onClose } : CreateGuardModalProps ) {
+    const [selectedTime, setSelectedTime] = useState<string>('Mañana');
+
+    const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedTime(event.target.value);
+    };
+
+    if (!isOpen) return null;
+      
+    return (
+        <div className="modal-guard">
+            <div className="modal-guard-content">
+                <div className="modal-guard-header">
+                    <h2>
+                        <button className="icon-button" onClick={onClose}><FaAngleLeft /></button> Registrar guardia
+                    </h2>                    
+                </div>
+                <div className="modal-guard-body">
+                    <input className="form-control" type="text" placeholder="Buscar por Nombre o CI" />
+
+                    <div><b>Nombre completo:</b></div>
+                    <div><b>CI:</b></div>
+                    <div>
+                        <b>TURNO:</b>
+                        <div className="options-container">
+                            <label className="radio-label">
+                                <input type="radio" name="time" value="Mañana" checked={selectedTime === 'Mañana'} onChange={handleTimeChange}/> Mañana
+                            </label>
+                            <label className="radio-label">
+                                <input type="radio" name="time" value="Tarde" checked={selectedTime === 'Tarde'} onChange={handleTimeChange}/> Tarde
+                            </label>
+                            <label className="radio-label">
+                                <input type="radio" name="time" value="Noche" checked={selectedTime === 'Noche'} onChange={handleTimeChange}/> Noche
+                            </label>
+                        </div>                  
+                    </div>   
+                    <button className="btn btn-blue">Registrar</button>              
+                </div>       
+            </div>
+        </div>
+    );
+}
--- CB-24-Proyecto-FAB-Frontend/src/components/sar/guard-management/guard-management.tsx
+++ CB-24-Proyecto-FAB-Frontend/src/components/sar/guard-management/guard-management.tsx
@@ -1,11 +1,53 @@
+import { CiSquarePlus } from "react-icons/ci";
 import LayoutSar from "../layout-sar/layout-sar"
 import "./guard-management.css"
+import { IoEllipsisVerticalCircle } from "react-icons/io5";
+import CreateGuardModal from "../create-guard-modal/create-guard-modal";
+import { useState } from "react";
 
 export default function GuardManagement() {
-    
-    return(
-        <LayoutSar>
-            <h2>Gestion Guardias </h2>
-        </LayoutSar>
-    );    
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const data = [
+      { turno: 'Mañana', ci: '123456', comnre: 'Juan Perez', estado: 'Activo' },
+      { turno: 'Tarde', ci: '789012', comnre: 'Maria Lopez', estado: 'Inactivo' },
+      // Agrega más datos si es necesario
+    ];
+  return(
+    <LayoutSar>
+      <div className="guard-container">
+        <h2 className="guard-title">GUARDIA</h2>
+        <div className="guard-header">
+          <button className="icon-button" onClick={() => setModalOpen(true)}><CiSquarePlus /></button>
+          <input className="form-control guard" type="date" id="fecha" />
+          <input className="form-control guard" type="text" placeholder="Buscar..." />
+        </div>
+        <table className="guard-table">
+          <thead>
+            <tr>
+              <th style={{ width: '40%' }}>Turno</th>
+              <th style={{ width: '15%' }}>CI</th>
+              <th style={{ width: '25%' }}>Nombre</th>
+              <th style={{ width: '15%' }}>Estado</th>
+              <th style={{ width: '5%' }}>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.turno}</td>
+                <td>{item.ci}</td>
+                <td>{item.comnre}</td>
+                <td>{item.estado}</td>
+                <td>
+                  <button className="guard options-button"><IoEllipsisVerticalCircle /></button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <CreateGuardModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+    </LayoutSar>      
+  );    
 }
