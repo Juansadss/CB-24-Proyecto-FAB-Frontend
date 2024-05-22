91785949f64b053cd943db9195ae8e6a59703d0c
Luis Mancilla <mcl0031064@est.univalle.edu>
2024-05-22 17:11:34 -0400

    Redireccionamiento

--- CB-24-Proyecto-FAB-Frontend/src/components/maintenance/maintenance-menu/maintenance-menu.tsx
+++ CB-24-Proyecto-FAB-Frontend/src/components/maintenance/maintenance-menu/maintenance-menu.tsx
@@ -24,7 +24,7 @@ export default function MaintenanceMenu() {
                 <h1>Fuerza Aérea</h1>
                 <span>
                     <div className="menu-options main-options">
-                        <Link className="option" to={""}>
+                        <Link className="option" to={"/menu-principal/mantenimiento/ordenes-trabajo"}>
                             <p>
                                 <GoGear fontSize={52} />
                                 <span>PROGRAMA DE<br />MANTENIMIENTO</span>
@@ -36,7 +36,7 @@ export default function MaintenanceMenu() {
                                 <span>ESTADO DE<br />AERONAVES</span>
                             </p>
                         </Link>
-                        <Link className="option" to={"/"}>
+                        <Link className="option" to={"/menu-principal/mantenimiento/control-componentes"}>
                             <p>
                                 <TbChecklist fontSize={62} />
                                 <span>CONTROL DE<br />COMPONENTES</span>
