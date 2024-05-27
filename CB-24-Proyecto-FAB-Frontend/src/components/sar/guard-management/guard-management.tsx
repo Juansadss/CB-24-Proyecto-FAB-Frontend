cb6cf615c4549c6f2e7e65ebb0c4fb106e6a9788
Rafael Gamboa <gpr0028293@est.univalle.edu>
2024-05-27 09:57:47 -0400

    Elementos de barra de busqueda

--- CB-24-Proyecto-FAB-Frontend/src/components/sar/guard-management/guard-management.tsx
+++ CB-24-Proyecto-FAB-Frontend/src/components/sar/guard-management/guard-management.tsx
@@ -4,23 +4,45 @@ import "./guard-management.css"
 import { IoEllipsisVerticalCircle } from "react-icons/io5";
 import CreateGuardModal from "../create-guard-modal/create-guard-modal";
 import { useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import DropdownInputSearch from "../dropdown-input-search/dropdown-input-search";
+import '../search-bar-styles.css'
+
+const options = [
+  { value: 'opcion1', label: 'Opción 1' },
+  { value: 'opcion2', label: 'Opción 2' },
+  { value: 'opcion3', label: 'Opción 3' },
+];
 
 export default function GuardManagement() {
   const [modalOpen, setModalOpen] = useState(false);
 
   const data = [
-      { turno: 'Mañana', ci: '123456', comnre: 'Juan Perez', estado: 'Activo' },
-      { turno: 'Tarde', ci: '789012', comnre: 'Maria Lopez', estado: 'Inactivo' },
-      // Agrega más datos si es necesario
-    ];
-  return(
+    { turno: 'Mañana', ci: '123456', comnre: 'Juan Perez', estado: 'Activo' },
+    { turno: 'Tarde', ci: '789012', comnre: 'Maria Lopez', estado: 'Inactivo' },
+  ];
+
+  return (
     <LayoutSar>
       <div className="guard-container">
         <h2 className="guard-title">GUARDIA</h2>
         <div className="guard-header">
           <button className="icon-button" onClick={() => setModalOpen(true)}><CiSquarePlus /></button>
-          <input className="form-control guard" type="date" id="fecha" />
-          <input className="form-control guard" type="text" placeholder="Buscar..." />
+          <form action="" className="search-bar">
+            <DropdownInputSearch options={options} />
+            <div className="search-bar-item">
+              <input type="date"  placeholder="Buscar..." className="search-input" />
+              <button className="search-button" >
+                <FaMagnifyingGlass />
+              </button>
+            </div>
+            <div className="search-bar-item">
+              <input type="text" placeholder="Buscar..." className="search-input" />
+              <button className="search-button">
+                <FaMagnifyingGlass />
+              </button>
+            </div>
+          </form> 
         </div>
         <table className="guard-table">
           <thead>
@@ -48,6 +70,6 @@ export default function GuardManagement() {
         </table>
       </div>
       <CreateGuardModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
-    </LayoutSar>      
-  );    
+    </LayoutSar>
+  );
 }
